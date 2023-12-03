0fc0b41813c59eb11533ac97d70a24be052977c6
AkhilReddy9595 <akhilreddy5995@gmail.com>
2023-12-03 23:58:26 +0530

    ecom store assignment

---
+++ src/Redux/productReducer.js
@@ -0,0 +1,36 @@
+// const initialState = {
+//   products: [],
+// };
+
+// const productsReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "FETCH_PRODUCTS":
+//       return {
+//         ...state,
+//         products: [...state.products, ...action.payload],
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default productsReducer;
+
+// productsReducer.js
+const initialState = {
+  products: [],
+};
+
+const productsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_PRODUCTS":
+      return {
+        ...state,
+        products: [...state.products, ...action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
